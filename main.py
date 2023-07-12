d27ac5ae39f7709980f887b528488b7df8f4e36f
ahalikoguz <ahalikoguz@gmail.com>
2023-07-12 10:35:09 +0300

    Commit 2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hello world")
-print("by bye happiness")
+import pandas as pd
+
+data = pd.read_csv(r"iris.data",)
+
+data.loc[data['species'] == 'Iris-setosa', 'species'] = 'apple'
+data.loc[data['species'] == 'Iris-versicolor', 'species'] = 'cherry'
+data.loc[data['species'] == 'Iris-virginica', 'species'] = 'blueberry'
