61a81f914af4dca2849c877e6d53c4434cd2d4de
ahalikoguz <ahalikoguz@gmail.com>
2023-07-12 13:08:45 +0300

    Commit 3

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-data = pd.read_csv(r"iris.data",)
+data = pd.read_csv(r"iris.data", header=None)
 
-data.loc[data['species'] == 'Iris-setosa', 'species'] = 'apple'
-data.loc[data['species'] == 'Iris-versicolor', 'species'] = 'cherry'
-data.loc[data['species'] == 'Iris-virginica', 'species'] = 'blueberry'
+data[4] = data[4].replace("Iris-setosa", "1")
+data[4] = data[4].replace("Iris-versicolor", "2")
+data[4] = data[4].replace("Iris-virginica", "3")
+
+data_shuffled = data.sample(frac=1)  # shuffle
